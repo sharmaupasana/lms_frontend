48d7b1c5220dc9ef58f75167b19fe04a9dc576aa
sharmaupasana <sharmaupasana493@gmail.com>
2025-03-19 15:58:01 +0545

    Changes

--- src/pages/department.jsx
+++ src/pages/department.jsx
@@ -1,30 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import PageTitle from '../components/pageTitle'
-import { getRequest } from '../services/apiService'
+import { getRequest, postRequest } from '../services/apiService'
 
 function Department() {
 
     const [datas, setDatas] = useState([])
+    const [inputName, setInputName] = useState('')
 
-    const getData = async() => {
+    const getData = async () => {
         var tempData = await getRequest('department')
         setDatas(tempData['departments'])
     }
 
+    const addData = async() => {
+        await postRequest('add-department', {departmentName: inputName})
+        setInputName('')
+    }
 
     useEffect(
         () => {
             getData()
         },
-        []
+        [datas]
     )
 
     return (
         <div className='flex flex-col gap-5'>
             <PageTitle title={'Department'} />
 
-            <div className='flex gap-5'>
+            <div className='flex flex-col gap-5'>
 
+                {/* Adding Data */}
+                <div className='bg-white flex flex-col gap-2 p-4 shadow-md rounded w-full'>
+                    <h2>Add Department</h2>
+                    <input value={inputName} onInput={(e) => setInputName(e.target.value)} type="text" placeholder='Title' className='border p-2 rounded-md border-gray-300 w-full' />
+                    <div className='flex flex-wrap'>
+                        <button onClick={() => addData()} className='bg-primary px-10 text-white py-2 rounded'>Add</button>
+                    </div>
+                </div>
+
+                {/* Data Indexing */}
                 <table className='bg-white shadow-md rounded w-full'>
                     <thead className='border-b border-b-primary'>
                         <tr>
@@ -40,7 +55,7 @@ function Department() {
                                         <td className="p-4">{data.name}</td>
                                         <td className="p-4 flex gap-2">
                                             <button className='bg-primary text-white px-4 py-1 rounded'>Edit</button>
-                                            <button className='bg-red-500 text-white px-4 py-1 rounded'>Delete</button>
+                                            <button onClick={() => { getRequest(`delete-department/${data.id}`) }} className='bg-red-500 text-white px-4 py-1 rounded'>Delete</button>
                                         </td>
                                     </tr>
                                 )
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -3,6 +3,20 @@ import { API_BASE_URL } from '../constants/utils';
 export async function getRequest(url) {
     var finalUrl = `${API_BASE_URL}${url}`
     var datas = await fetch(finalUrl)
-    var actualData = await datas.json()
-    return actualData
+    var response = await datas.json()
+    return response
 }   
+
+export async function postRequest(url, data) {
+    var finalUrl = `${API_BASE_URL}${url}`
+    var datas = await fetch(finalUrl,{
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(data),
+    })
+    var response = await datas.json()
+    return response
+}
